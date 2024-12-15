c91411f1e118db38b3206526784cda77da464563
sh78ba <sh78ba@gmail.com>
2024-12-15 09:24:53 +0530

    fixed

--- frontend/src/pages/user/checkout.jsx
+++ frontend/src/pages/user/checkout.jsx
@@ -177,8 +177,8 @@ const Checkout = () => {
     }
  
     try {
-      //https://project-task-backend.vercel.app
-      const response = await fetch("http://localhost:6001/create-checkout-session", {
+      //bakend code for stripe
+      const response = await fetch("https://project-task-backend.vercel.app/create-checkout-session", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
