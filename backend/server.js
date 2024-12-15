d9ab3e7d084109107aa9a453511867156008db86
sh78ba <sh78ba@gmail.com>
2024-12-15 08:57:06 +0530

    dd

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,6 @@ app.post("/create-checkout-session", async (req, res) => {
     try {
         const { productsOrdered } = req.body;
 
-        if (!Array.isArray(productsOrdered) || productsOrdered.length === 0) {
-            return res.status(400).json({ error: "Items array is required" });
-        }
 
         const lineItems = productsOrdered.map((item) => ({
             price_data: {
